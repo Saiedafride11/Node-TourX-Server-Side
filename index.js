919fc0affe83d6bff67b194943e582177b26d7aa
Coders Key <designer.afride@gmail.com>
2021-10-30 14:38:28 -0700

    space checke

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u9lnx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
