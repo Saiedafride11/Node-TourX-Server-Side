14991ac1faea296483d723dbe9edffc4b89a0812
Coders Key <designer.afride@gmail.com>
2021-10-30 02:15:36 -0700

    order deleted added

--- index.js
+++ index.js
@@ -2,21 +2,76 @@ const express = require('express')
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
-// const bodyParser = require('body-parser');
 const ObjectId = require('mongodb').ObjectId;
 
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors())
-// app.use(bodyParser.json())
-// or
 app.use(express.json())
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u9lnx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("tourX");
+    const toursCollection = database.collection("tours");
+    const ordersCollection = database.collection("orders");
+    
+     //GET API Tours
+     app.get('/tours', async (req, res) => {
+      const cursor = toursCollection.find({})
+      const tours = await cursor.toArray();
+      res.send(tours)
+    })
+
+    // POST API Tours
+    app.post('/orders', async (req, res) => {
+        const order = req.body;
+        const result = await ordersCollection.insertOne(order)
+        res.json(result)
+    })
+
+    // ---------------------------------------------------------
+    // ------------------- Orders  -----------------------------
+    // ---------------------------------------------------------
+
+    //GET API Tours
+    app.get('/orders', async (req, res) => {
+      const cursor = ordersCollection.find({})
+      const orders = await cursor.toArray()
+      res.send(orders)
+    })
+
+    // GET API Tours Id
+    app.get('/orders/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id)};
+      const tour = await ordersCollection.findOne(query);
+      res.send(tour);
+    })
+
+    // Delete API Tours Id
+    app.delete('/orders/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id)};
+      const tour = await ordersCollection.deleteOne(query);
+      res.json(tour);
+    })
+
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello TourX!')
   })
   
   app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log('Running TourX Server', port)
   })
